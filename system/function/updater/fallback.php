8fcf9f5e9bea459867901f17726c4d00da7cb167
kookxiang <kookxiang@qq.com>
2013-08-19 15:23:20 +0800

    把安装脚本也内置进更新系统

--- system/function/updater/fallback.php
+++ system/function/updater/fallback.php
@@ -1,11 +1,17 @@
 <?php
 if(!defined('IN_KKFRAME')) exit('Access Denied');
-$usr = DB::fetch_first("SELECT * FROM member LIMIT 0,1");
-$member_field = array_keys($usr);
-if(!in_array('sign_method', $member_field)){
+$query = DB::query('SHOW TABLES');
+$tables = array();
+while($table = DB::fetch($query)){
+	$tables[] = implode('', $table);
+}
+
+if(!in_array('setting', $tables)){
 	include SYSTEM_ROOT.'./function/updater/1.0.php';
-}else{
+}elseif(!in_array('member_setting', $tables)){
 	include SYSTEM_ROOT.'./function/updater/1.13.8.18.php';
+}elseif(!in_array('member', $tables)){
+	include SYSTEM_ROOT.'./function/updater/install.php';
 }
 error::system_error('自动更新“贴吧签到助手”失败，请尝试手动安装');
 ?>
