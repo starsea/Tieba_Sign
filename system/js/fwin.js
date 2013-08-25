4b3bc4982af0efb1a6bd57ee9277a8bca467e831
kookxiang <kookxiang@qq.com>
2013-08-25 19:15:34 +0800

    修正loading窗口不消失的问题

--- system/js/fwin.js
+++ system/js/fwin.js
@@ -69,7 +69,7 @@ function msg_win_action(link){
 		$('.loading-icon').finish();
 		$('.loading-icon').fadeOut();
 		createWindow().setTitle('系统消息').setContent(result.msg).addCloseButton('确定').append();
-	}).fail(function() { createWindow().setTitle('系统错误').setContent('发生未知错误: 无法解析返回结果').addCloseButton('确定').append(); });
+	}).fail(function() { $('.loading-icon').finish(); $('.loading-icon').fadeOut(); createWindow().setTitle('系统错误').setContent('发生未知错误: 无法解析返回结果').addCloseButton('确定').append(); });
 	return false;
 }
 function msg_redirect_action(link){
@@ -81,6 +81,6 @@ function msg_redirect_action(link){
 		$('.loading-icon').finish();
 		$('.loading-icon').fadeOut();
 		createWindow().setTitle('系统消息').setContent(result.msg).addButton('确定', function(){ location.href = result.redirect; }).append();
-	}).fail(function() { createWindow().setTitle('系统错误').setContent('发生未知错误: 无法解析返回结果').addCloseButton('确定').append(); });
+	}).fail(function() { $('.loading-icon').finish(); $('.loading-icon').fadeOut(); createWindow().setTitle('系统错误').setContent('发生未知错误: 无法解析返回结果').addCloseButton('确定').append(); });
 	return false;
 }
