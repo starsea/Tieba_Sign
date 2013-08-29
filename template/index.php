92b5cf09e1e1f2df77e46ee8480ed7e4d3459f49
kookxiang <kookxiang@qq.com>
2013-08-29 09:14:09 +0800

    上移 刷新喜欢贴吧 链接

--- template/index.php
+++ template/index.php
@@ -44,11 +44,11 @@ foreach ($users as $_uid => $username){
 <div class="main-content">
 <div id="content-loved-tb" class="hidden">
 <h2>我喜欢的贴吧</h2>
+<p>如果此处显示的贴吧有缺失，请<a href="index.php?action=refresh_liked_tieba" onclick="return msg_redirect_action(this.href+'&formhash='+formhash)">点此刷新喜欢的贴吧</a>.</p>
 <table>
 <thead><tr><td style="width: 40px">序号</td><td>贴吧</td><td style="width: 65px">忽略签到</td></tr></thead>
 <tbody></tbody>
 </table>
-<p>如果此处显示的贴吧有缺失，请<a href="index.php?action=refresh_liked_tieba" onclick="return msg_redirect_action(this.href+'&formhash='+formhash)">点此刷新喜欢的贴吧</a>.</p>
 </div>
 <div id="content-sign-log" class="hidden">
 <h2>签到记录</h2>
