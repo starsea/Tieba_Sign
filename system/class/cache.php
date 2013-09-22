8ebca41213a53303af410b1bd835a21057b85b72
kookxiang <kookxiang@qq.com>
2013-09-22 16:25:10 +0800

    增加 mamcached 支持

--- system/class/cache.php
+++ system/class/cache.php
@@ -5,16 +5,25 @@ class CACHE{
 	public static function get($key){
 		static $_cache = array();
 		if(isset($_cache[$key])) return $_cache[$key];
-		$query = DB::query("SELECT v FROM cache WHERE k='{$key}'", 'SILENT');
-		$_cache[$key] = unserialize(DB::fetch($query));
+		if(MEMCACHE::isAvailable()){
+			$_cache[$key] = MEMCACHE::get($key);
+		}else{
+			$query = DB::query("SELECT v FROM cache WHERE k='{$key}'", 'SILENT');
+			$result = DB::fetch($query);
+			$_cache[$key] = unserialize($result['v']);
+		}
 		if(!$_cache[$key]){
 			return $_cache[$key] = self::update($key);
 		}
 		return $_cache[$key];
 	}
 	public static function save($key, $value){
-		$value = addslashes(serialize($value));
-		DB::query("REPLACE INTO cache SET k='{$key}', v='{$value}'", 'SILENT');
+		if(MEMCACHE::isAvailable()){
+			MEMCACHE::save($key, $value);
+		}else{
+			$value = addslashes(serialize($value));
+			DB::query("REPLACE INTO cache SET k='{$key}', v='{$value}'", 'SILENT');
+		}
 	}
 	public static function update($key){
 		$builder_file = SYSTEM_ROOT."./function/cache/cache_{$key}.php";
@@ -26,7 +35,46 @@ class CACHE{
 		}
 	}
 	public static function clear(){
+		MEMCACHE::clear();
 		DB::query("TRUNCATE TABLE cache", 'SILENT');
+	}
+}
+
+class MEMCACHE{
+	public function isAvailable(){
+		$object = MEMCACHE::object();
+		if(!$object) return false;
+		if($object->get('test')) return true;
+		$object->set('test', '1');
+		return $object->get('test');
+	}
+	public function object(){
+		static $obj;
+		if(defined('MEMCACHE_INITED')) return $obj;
+		$obj = null;
+		if($_SERVER['HTTP_APPVERSION']){
+			$obj = memcache_init();
+		}elseif($_SERVER['USER'] == 'bae'){
+			require_once 'BaeMemcache.class.php';
+			$obj = new BaeMemcache();
+		}
+		define('MEMCACHE_INITED', 'true');
+		return $obj;
+	}
+	function clear(){
+		$obj = MEMCACHE::object();
+		if(!$obj) return;
+		return $obj->clear();
+	}
+	function get($key){
+		$obj = MEMCACHE::object();
+		if(!$obj) return;
+		return $obj->get($key);
+	}
+	function save($key, $value, $exp = 3600){
+		$obj = MEMCACHE::object();
+		if(!$obj) return;
+		return $obj->set($key, $value, $exp);
 	}
 }
 
