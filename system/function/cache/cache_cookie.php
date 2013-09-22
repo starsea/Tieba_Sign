9e157ac994d9f340c2515052c82f7eb7271a10f8
kookxiang <kookxiang@qq.com>
2013-09-22 18:05:53 +0800

    修正cookie缓存获取bug

--- system/function/cache/cache_cookie.php
+++ system/function/cache/cache_cookie.php
@@ -1,7 +1,7 @@
 <?php
 if(!defined('IN_KKFRAME')) exit();
 
-$query = DB::query("SELECT uid, cookie FROM member WHERE uid='{$uid}'");
+$query = DB::query("SELECT uid, cookie FROM member");
 while($result = DB::fetch($query)){
 	$cache[ $result['uid'] ] = $result['cookie'];
 }
