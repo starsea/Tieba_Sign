5ad762bdb0b26c6d4ab8dc62f441584cb81186cf
kookxiang <kookxiang@qq.com>
2013-09-24 13:20:45 +0800

    Build 1.13.9.23

--- system/function/updater/1.13.9.8.php
+++ system/function/updater/1.13.9.8.php
@@ -2,9 +2,9 @@
 if(!defined('IN_KKFRAME')) exit('Access Denied');
 DB::query('CREATE TABLE IF NOT EXISTS `cache` (
   `k` varchar(32) NOT NULL,
-  `v` varchar(1024) NOT NULL,
+  `v` TEXT NOT NULL,
   PRIMARY KEY (`k`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8');
-saveSetting('version', '1.13.9.22');
-showmessage('成功更新到 1.13.9.22！', './', 1);
+saveSetting('version', '1.13.9.23');
+showmessage('成功更新到 1.13.9.23！', './', 1);
 ?>
