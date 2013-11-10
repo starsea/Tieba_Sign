aa93474dd2cf4fb3224e22550c3379c11595525d
kookxiang <i@ikk.me>
2013-11-10 09:16:24 +0800

    重构安装脚本

--- system/function/updater/install.php
+++ system/function/updater/install.php
@@ -15,22 +15,30 @@ CREATE TABLE IF NOT EXISTS `cron` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT IGNORE INTO `cron` (`id`, `enabled`, `nextrun`, `order`) VALUES
+('daily', 0, 0, 0),
+('ext_sign', 1, 0, 50),
+('mail', 1, 0, 100),
+('sign', 1, 0, 20),
+('sign_retry', 1, 0, 110),
+('update_tieba', 1, 0, 10);
+
 CREATE TABLE IF NOT EXISTS `mail_queue` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `to` varchar(255) NOT NULL,
   `subject` varchar(255) NOT NULL,
   `content` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `member` (
   `uid` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(12) NOT NULL,
+  `username` varchar(24) DEFAULT NULL,
   `password` varchar(32) NOT NULL,
   `email` varchar(32) NOT NULL,
   `cookie` text NOT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `member_bind` (
   `uid` int(10) unsigned NOT NULL,
@@ -41,8 +49,6 @@ CREATE TABLE IF NOT EXISTS `member_bind` (
 
 CREATE TABLE IF NOT EXISTS `member_setting` (
   `uid` int(10) unsigned NOT NULL,
-  `use_bdbowser` tinyint(1) NOT NULL DEFAULT '1',
-  `sign_method` tinyint(1) NOT NULL DEFAULT '1',
   `error_mail` tinyint(1) NOT NULL DEFAULT '1',
   `send_mail` tinyint(1) NOT NULL DEFAULT '0',
   `zhidao_sign` tinyint(1) NOT NULL DEFAULT '0',
@@ -58,8 +64,20 @@ CREATE TABLE IF NOT EXISTS `my_tieba` (
   `unicode_name` varchar(512) NOT NULL,
   `skiped` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`tid`),
-  UNIQUE KEY `name` (`uid`,`name`)
+  KEY `uid` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `plugin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `module` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+INSERT IGNORE INTO `plugin` (`id`, `name`, `module`) VALUES
+(1, 'debug_info', ''),
+(2, 'update_log', '');
 
 CREATE TABLE IF NOT EXISTS `setting` (
   `k` varchar(32) NOT NULL,
@@ -77,13 +95,6 @@ CREATE TABLE IF NOT EXISTS `sign_log` (
   UNIQUE KEY `tid` (`tid`,`date`),
   KEY `uid` (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `cron` (`id`, `enabled`, `nextrun`, `order`) VALUES
-('daily', 1, 0, 0),
-('update_tieba', 1, 0, 10),
-('sign', 1, 0, 20),
-('ext_sign', 1, 0, 50),
-('mail', 1, 0, 100);
 EOF;
 
 $sql_array = explode(';', $sql);
@@ -92,6 +103,6 @@ foreach ($sql_array as $sql){
 	if($sql) DB::query($sql);
 }
 
-saveSetting('version', '1.13.10.4');
-showmessage('1.13.10.4 安装成功！', './', 1);
+saveSetting('version', '1.13.11.9');
+showmessage('1.13.11.9 安装成功！', './', 1);
 ?>
