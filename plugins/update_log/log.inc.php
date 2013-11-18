992e6b8dbe5f6ad43d611928089437034c671273
kookxiang <i@ikk.me>
2013-11-18 14:18:58 +0800

    解决https下更新日志的问题

--- plugins/update_log/log.inc.php
+++ plugins/update_log/log.inc.php
@@ -2,4 +2,4 @@
 if(!defined('IN_KKFRAME')) exit('Access Denied!');
 ?>
 <h2>新功能推荐</h2>
-<script src="http://api.ikk.me/tieba_sign_update_log.js?<?php echo random(8); ?>"></script>
+<script src="//api.ikk.me/tieba_sign_update_log.js?<?php echo random(8); ?>"></script>
